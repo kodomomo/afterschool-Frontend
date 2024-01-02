d2e19d4de22b3723d157f21607a9b027c40969f8
김한비 <gksql2256@dsm.hs.kr>
2024-01-02 19:22:21 +0900

    feat :: header publishing

---
+++ afterschool-frontend/src/components/header/index.tsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { Logo } from "../../assets";
+
+const Header = () => {
+
+  return (
+    <Container>
+      <img
+        src={Logo}
+        alt="logo"
+      />
+      <Div><Text>수강 신청 내역</Text>
+      <Name>권하은 님</Name></Div>
+      
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background: #fff;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.15);
+  height: 50px;
+  width: 100%;
+  display: flex;
+  padding: 10px 7%;
+  align-items: center;
+  img {
+    cursor: pointer;
+  }
+  margin-bottom: 40px;
+`;
+
+const Text = styled.div`
+  color: #000;
+  font-size: 18px;
+  font-weight: 500;
+  margin-right: 88px;
+  font-family: Pretendard;
+
+`;
+
+const Name = styled.div`
+  color: #666;
+  font-size: 18px;
+  font-weight: 500;
+  font-family: Pretendard;
+`;
+
+const Div=styled.div`
+margin-left: auto;
+display: flex;
+
+`
+
+export default Header;
